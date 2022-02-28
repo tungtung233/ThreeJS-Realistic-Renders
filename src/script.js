73cbd89ca9a602adfd61a779e74eb64ac4715217
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-28 00:39:49 -0500

    add environment map to helmet

--- src/script.js
+++ src/script.js
@@ -13,12 +13,26 @@ const cubeTextureLoader = new THREE.CubeTextureLoader();
  */
 // Debug
 const gui = new dat.GUI();
+const debugObject = {};
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
 // Scene
 const scene = new THREE.Scene();
+
+// Update all materials
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      child.material.envMap = environmentMap;
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+    }
+  });
+};
 
 // Environment Map
 const environmentMap = cubeTextureLoader.load([
@@ -30,6 +44,14 @@ const environmentMap = cubeTextureLoader.load([
   '/textures/environmentMaps/0/nz.jpg',
 ]);
 scene.background = environmentMap;
+
+debugObject.envMapIntensity = 5;
+gui
+  .add(debugObject, 'envMapIntensity')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .onChange(updateAllMaterials);
 
 // Models
 gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
@@ -44,6 +66,8 @@ gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
     .max(Math.PI)
     .step(0.001)
     .name('rotation');
+
+  updateAllMaterials();
 });
 
 // Lights
